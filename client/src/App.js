72150d592069cd3b51256a99b7a78e19c6c52c1a
Madhvik <madhu.y94@gmail.com>
2019-12-04 18:07:54 +0100

    done

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,23 @@
 import React from "react";
 import Chat from "./components/Chat";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Route
+        exact
+        path="/signup"
+         />}
+      />
+      <Route
+        exact
+        path="/login"
+         />}
+      />
+    </div>
+  );
 }
 
 export default App;
