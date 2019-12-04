69936c224c0e3c15760553d8d03ed0ca143f2c85
Floriano Albertini <albertini@Florianos-MacBook-Pro.local>
2019-12-04 18:28:56 +0100

    chat

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,23 @@
-import React from "react";
-import Chat from "./components/Chat";
-import { Route, Redirect } from "react-router-dom";
+import React, { Component } from "react";
+import "./App.css";
+import { Route, Switch } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+import Chat from "./components/Chat";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-class App extends React.Component {
-  state = {
-    user: this.props.user
-  };
-
+class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navbar></Navbar>
-
-
+        <Navbar />
+        <Switch>
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={Signup} />
+          <Route exact path="/chat" component={Chat} />
+        </Switch>
       </div>
     );
   }
