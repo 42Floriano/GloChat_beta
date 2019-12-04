38b3cf265039ddff178dac0a89e10e1577fe2c14
Madhvik <madhu.y94@gmail.com>
2019-12-04 15:13:12 +0100

    models done

---
+++ models/Message.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const messageSchema = new Schema({
+  text: String,
+  language: String,
+  lastActiveAt: Date,
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User"
+  }
+});
+
+const Message = mongoose.model("Message", messageSchema);
+module.exports = Message;
