e1826651aeeb7554e739bf0a59656e7f2b5d17de
Davide Bernocchi <davide.berno84@gmail.com>
2019-12-04 14:11:12 +0100

    project setup

--- app.js
+++ app.js
@@ -13,7 +13,9 @@ const MongoStore = require("connect-mongo")(session);
 const flash = require("connect-flash");
 
 mongoose
-  .connect("mongodb://localhost/glochat", { useNewUrlParser: true })
+  .connect(process.env.MONGODB_URI || "mongodb://localhost/glochat", {
+    useNewUrlParser: true
+  })
   .then(x => {
     console.log(
       `Connected to Mongo! Database name: "${x.connections[0].name}"`
