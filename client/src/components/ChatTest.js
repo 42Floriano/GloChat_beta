1e94090961eb88147284674de5c7628c9b863bf4
Madhvik <madhu.y94@gmail.com>
2019-12-10 14:37:26 +0100

    done

--- client/src/components/ChatTest.js
+++ client/src/components/ChatTest.js
@@ -7,13 +7,14 @@ class ChatTest extends Component {
   state = {
     message: "",
     profilePic: "",
+
     user: this.props.user,
     rooms: [],
     messages: [],
     socketId: ""
   };
 
-  handleRoomSubit(event) {
+  handleRoomSubmit(event) {
     event.preventDefault();
     axios
       .post("/room", { room: "ROOM" })
@@ -37,49 +38,49 @@ class ChatTest extends Component {
   render() {
     console.log("hello", this.state.profilePic);
     return (
-      <div className="container">
-        <div
-          className="row mt-4"
-          style={{
-            height: "550px"
-          }}
-        >
-          <div className="col-sm-3 bg-primary">
-            <h2>Rooms</h2>
-            <form onSubmit={this.handleRoomSubit}>
-              <button className="btn btn-light" type="submit">
-                Generate a room
-              </button>
-            </form>
-            <div>
-              {this.state.rooms.map(room => {
-                return (
-                  <p>
-                    <Link className="text-white" to={`/room/${room._id}`}>
-                      {room._id}
-                    </Link>
-                  </p>
-                );
-              })}
-            </div>
-          </div>
-          <Room user={this.state.user} />
+      <div>
+        <div>
+          <img
+            src={this.state.profilePic}
+            style={{
+              border: "2px solid black",
+              margin: "30px  20px",
+
+              width: "10vw"
+            }}
+            alt="profile"
+          />
         </div>
 
-        <img
-          src={this.state.profilePic}
-          style={{
-            border: "2px solid black",
-            margin: "30px  20px",
-            display: "flex",
-            float: "right",
-            width: "10%",
-            position: "fixed",
-            bottom: "65%",
-            left: "85%"
-          }}
-          alt="profile"
-        />
+        <div className="container">
+          <div
+            className="row mt-4"
+            style={{
+              height: "550px"
+            }}
+          >
+            <div className="col-sm-3 bg-primary">
+              <h2>Rooms</h2>
+              <form onSubmit={this.handleRoomSubmit}>
+                <button className="btn btn-light" type="submit">
+                  Generate a room
+                </button>
+              </form>
+              <div>
+                {this.state.rooms.map(room => {
+                  return (
+                    <p>
+                      <Link className="text-white" to={`/room/${room._id}`}>
+                        {room._id}
+                      </Link>
+                    </p>
+                  );
+                })}
+              </div>
+            </div>
+            <Room user={this.state.user} />
+          </div>
+        </div>
       </div>
     );
   }
