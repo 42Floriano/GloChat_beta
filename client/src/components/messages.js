97b25bdf9551ec8c1914340b888ed78d89fd2e8e
davideBerno <davide.berno84@gmail.com>
2019-12-10 12:48:09 +0100

    msg

---
+++ client/src/components/messages.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import Message from "./Message";
+
+class Messages extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default Messages;
