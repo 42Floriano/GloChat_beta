65940cccb457913ff2d127bcc696f90ae737f141
Floriano Albertini <albertini@Florianos-MacBook-Pro.local>
2019-12-12 12:34:04 +0100

    done

---
+++ client/build/precache-manifest.c21f69b52fe976ee4f437d93c86efd68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "416050d61938e0ebef6483b2bcf113e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa4da2f399a3fadd0649",
+    "url": "/static/css/2.a1f70397.chunk.css"
+  },
+  {
+    "revision": "f76b829c0d283fb895ee",
+    "url": "/static/css/main.bd02f195.chunk.css"
+  },
+  {
+    "revision": "fa4da2f399a3fadd0649",
+    "url": "/static/js/2.be2d84f6.chunk.js"
+  },
+  {
+    "revision": "f76b829c0d283fb895ee",
+    "url": "/static/js/main.b3d0987c.chunk.js"
+  },
+  {
+    "revision": "1337678ae3301a2c8a7b",
+    "url": "/static/js/runtime-main.8eac158a.js"
+  }
+]);
