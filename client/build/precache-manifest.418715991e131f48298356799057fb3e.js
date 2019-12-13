95b645d90c3259a48f13f9750bedeaa5e060ddc0
Floriano Albertini <albertini@Florianos-MacBook-Pro.local>
2019-12-13 02:20:06 +0100

    final50

--- client/build/precache-manifest.418715991e131f48298356799057fb3e.js
+++ client/build/precache-manifest.418715991e131f48298356799057fb3e.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0a30bdf57aeb348d78ba3ef792354476",
+    "revision": "e5fa25966111d3a9bce2a42107bc4330",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.a1f70397.chunk.css"
   },
   {
-    "revision": "85a6b45be8521c09cf8d",
+    "revision": "6c32a8301a4367dbad29",
     "url": "/static/css/main.b2aa13f5.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.eef5b0a5.chunk.js"
   },
   {
-    "revision": "85a6b45be8521c09cf8d",
-    "url": "/static/js/main.f2b77119.chunk.js"
+    "revision": "6c32a8301a4367dbad29",
+    "url": "/static/js/main.8e291157.chunk.js"
   },
   {
     "revision": "1337678ae3301a2c8a7b",
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9b62f2c27f63a7d12bfd79793cba5121.js"
+  "/precache-manifest.418715991e131f48298356799057fb3e.js"
 );
 
 self.addEventListener('message', (event) => {
